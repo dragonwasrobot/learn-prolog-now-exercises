242568d6faaa5994a3cb4c447ea8960b0f91c8d6
Thomas Ba <git@t-battermann.de>
2015-03-04 18:27:10 +0100

    Fixed Typo

--- chapter-1/exercises.pl
+++ chapter-1/exercises.pl
@@ -11,7 +11,7 @@
 %% 1. vINCENT - atom
 %% 2. Footmassage - variable
 %% 3. variable23 - atom
-%% 4. variable2000 - atom
+%% 4. Variable2000 - variable
 %% 5. big_kahuna_burger - atom
 %% 6. 'big kahuna burger' - atom
 %% 7. big kahuna burger - neither
