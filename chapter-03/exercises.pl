cdf56dc3663108fffd035fa59d395e6367cde76e
Yuto Chikazawa <minet6759@gmail.com>
2019-07-07 20:36:29 +0900

    Fix `X` to anonymous variable in `greater_than`

--- chapter-03/exercises.pl
+++ chapter-03/exercises.pl
@@ -28,7 +28,7 @@ in(X,Y) :-
 %% ?- greater_than(succ(succ(succ(0))),succ(0)). -> true
 %% ?- greater_than(succ(succ(0)),succ(succ(succ(0)))). -> no
 
-greater_than(succ(X),0).
+greater_than(succ(_),0).
 greater_than(succ(X),succ(Y)) :-
   greater_than(X,Y).
 
